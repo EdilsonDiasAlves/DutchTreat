5b22807426f4ee3b7da49cce59fcd7ad25089037
Edilson Moizinho <edilsondiasalves@outlook.com>
2021-07-24 20:58:59 -0300

    Addition of javascript with jquery

---
+++ wwwroot/js/index.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+
+    console.log("Javascript loaded")
+
+    var theForm = $("#theForm");
+    theForm.hide();
+
+    let button = $("#buyButton");
+    button.on("click", () => console.log("Buying Item"));
+
+    let productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("You clicked on " + $(this).text());
+    });
+
+    let $loginToogle = $("#login-toggle");
+    let $popupForm = $(".popup-form");
+
+    $loginToogle.on("click", () => $popupForm.fadeToggle(600));
+
+});
