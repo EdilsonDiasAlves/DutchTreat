2a869b15ab86f814ca7c9bb7ff0d857d674a31e7
Edilson Moizinho <edilsondiasalves@outlook.com>
2021-08-09 01:19:19 -0300

    Implementing Entity Framework Core Configuration

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,8 @@
+using DutchTreat.Data;
 using DutchTreat.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
