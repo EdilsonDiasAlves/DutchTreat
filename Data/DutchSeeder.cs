a9e215e27e27bf1af4b802c2c7faf540f606638f
Edilson Moizinho <edilsondiasalves@outlook.com>
2021-08-22 01:55:52 -0300

    Begin the authentication and authorization with Asp.Net Core Entity 1. Adding user table and migrations 2. Basic configuration 3. Login form with basic validation (next -> logic implementation)

--- Data/DutchSeeder.cs
+++ Data/DutchSeeder.cs
@@ -1,9 +1,11 @@
 ﻿using DutchTreat.Data.Entities;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace DutchTreat.Data
@@ -12,16 +14,37 @@ namespace DutchTreat.Data
     {
         private readonly DutchContext _ctx;
         private readonly IWebHostEnvironment _env;
+        private readonly UserManager<StoreUser> _userManager;
 
-        public DutchSeeder(DutchContext ctx, IWebHostEnvironment env)
+        public DutchSeeder(DutchContext ctx, IWebHostEnvironment env, UserManager<StoreUser> userManager)
         {
             _ctx = ctx;
             _env = env;
+            _userManager = userManager;
         }
 
-        public void Seed()
+        public async Task SeedAsync()
         {
             _ctx.Database.EnsureCreated();
+
+            StoreUser user = await _userManager.FindByEmailAsync("admin@email.com");
+
+            if (user == null)
+            {
+                user = new StoreUser()
+                {
+                    FirstName = "admin",
+                    Email = "admin@email.com",
+                    UserName = "admin"
+                };
+
+                var result = await _userManager.CreateAsync(user, "P@ssw0rd");
+;
+                if (result != IdentityResult.Success)
+                {
+                    throw new InvalidOperationException("Error creating new user in seeder");
+                }
+            }
 
             if (!_ctx.Products.Any())
             {
@@ -29,8 +52,9 @@ namespace DutchTreat.Data
                 var json = File.ReadAllText(filePath);
                 var products = JsonSerializer.Deserialize<IEnumerable<Product>>(json);
 
-                var order = new Order()
+                var order = new Order() 
                 {
+                    User = user,
                     OrderDate = DateTime.Today,
                     OrderNumber = "10000",
                     Items = new List<OrderItem>()
--- Startup.cs
+++ Startup.cs
@@ -1,8 +1,10 @@
 using DutchTreat.Data;
 using DutchTreat.Data.Converter;
+using DutchTreat.Data.Entities;
 using DutchTreat.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -14,6 +16,12 @@ namespace DutchTreat
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddIdentity<StoreUser, IdentityRole>(cfg =>
+            {
+                cfg.User.RequireUniqueEmail = true;
+            }).AddEntityFrameworkStores<DutchContext>();
+
             // Dependency Injection
             services.AddDbContext<DutchContext>();
             services.AddTransient<DutchSeeder>();
@@ -44,6 +52,9 @@ namespace DutchTreat
 
             app.UseStaticFiles();
             app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(cfg =>
             {
